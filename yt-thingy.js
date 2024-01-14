4021682c65fc820ad29d41fbc6ae5e37baccd9ce
Reese Haywood <reese.haywood@gmail.com>
2024-01-13 23:37:27 -0500

    Update yt-thingy.js

--- yt-thingy.js
+++ yt-thingy.js
@@ -1,6 +1,7 @@
 (function(){
   console.log("running yt-thingy");
   let element = documemt.getElementsByTagName('ytd-enforcement-message-view-model');
+  console.log(element.length);
   element.remove();
   location.reload();
 })();
